d0c5859f4955dcbb6a5c9dd6971804097e921dbd
Silvia Parada <silviaparadag@yahoo.es>
2024-01-16 17:31:44 +0100

    Add some styles to basic structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,14 @@
 import '../styles/layout/Footer.scss';
-import SpLogo from '../assets/logo-sp-black.svg';
+// import SpLogo from '../assets/logo-sp-black.svg';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__grid">
-        <a href="/portfolio/">
+      {/* <div className="footer__grid">
+        <a href="https://silviaparadag.github.io/portfolio/">
           <img src={SpLogo} alt="Silvia Parada Logo" className="header__logo" />
         </a>
-        <div></div>
-      </div>
+      </div> */}
       <div className="footer__text">
         <p className="footer__text--p">© Silvia Parada | 2023</p>
         <p className="footer__text--italic">All rights reserved</p>
